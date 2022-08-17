209e61a1f62407f680fed5ae2fa3a2be034b6a93
Rickyflame <kimumuricky@gmail.com>
2022-08-17 05:08:02 +0300

    advanced tasks

--- sort.h
+++ sort.h
@@ -18,10 +18,11 @@ typedef enum bool
 } bool;
 
 /**
- * struct listint_s - doubly linked list node
- * @prev: pointer to previous element of the list
- * @next: pointer to the next element of the list
- * @n: integer value
+ * struct listint_s - Doubly linked list node
+ *
+ * @n: Integer stored in the node
+ * @prev: Pointer to the previous element of the list
+ * @next: Pointer to the next element of the list
  */
 typedef struct listint_s
 {
@@ -30,14 +31,20 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-void swap_ints(int *a, int *b);
-
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
 
 #endif
